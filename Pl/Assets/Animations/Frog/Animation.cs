9e9d7e59171c230420b0ec385891b7737e872de7
Leo <38lionman@gmail.com>
2023-07-18 00:14:20 +0300

    0000
    worked proj

---
+++ Pl/Assets/Animations/Frog/Animation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+            State = States.Idle;
+    }
+
+    public enum States
+    {
+        Idle
+    }
+    States State
+    {
+        get {return(States)anim.GetInteger("state");}
+        set {anim.SetInteger("state", (int)value);}
+    }
+}
