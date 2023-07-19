4dcfce8ccc44b60de7a0c3f613593c93b12ce826
chender335 <superrat345@gmail.com>
2023-07-19 23:35:43 +0300

    +spike ball

---
+++ Pl/Assets/Traps/SpikeBall/ObjectToMoveAround.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ObjectToMoveAround : MonoBehaviour
+{
+    private float angle;
+    private MoveAround moveInfo;
+    private float radius;
+
+    private void Awake()
+    {
+        moveInfo = transform.parent.GetComponent<MoveAround>();
+        angle = moveInfo.StratAngle * Mathf.Deg2Rad;
+        radius = Mathf.Sqrt(transform.localPosition.x * transform.localPosition.x + transform.localPosition.y * transform.localPosition.y);
+
+    }
+
+    private void Update()
+    {
+        angle -= moveInfo.Speed /*/ radius*/ * Time.deltaTime;
+
+        transform.localPosition = new Vector2(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle));
+    }
+}
