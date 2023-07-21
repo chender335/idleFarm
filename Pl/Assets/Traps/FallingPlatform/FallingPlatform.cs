00ca7e92d5e39eb5bc72fae6d0171f5c3c565a8b
chender335 <superrat345@gmail.com>
2023-07-22 00:15:43 +0300

    fan + fall

---
+++ Pl/Assets/Traps/FallingPlatform/FallingPlatform.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    [SerializeField] private float fallStartSpeed;
+    [SerializeField] private float fallAcceleration;
+    [SerializeField] private float fallDistance;
+    [SerializeField] private string fallAnimationName = "Fall";
+    [SerializeField] private float deltaYIfIdle;
+    [SerializeField] private float idleSpeed;
+
+    private float distancePassed;
+    private bool isFalling;
+    private float previousY;
+    private float curentSpeed;
+    private float startY;
+    private Vector2 moveDirection;
+
+    private Animator animator;
+    private Collider2D collider;
+
+    private void Awake()
+    {
+        previousY = transform.position.y;
+        moveDirection = Vector2.up;
+        startY = transform.position.y;
+        animator = GetComponent<Animator>();
+        curentSpeed = fallStartSpeed;
+        collider = GetComponent<Collider2D>();
+    }
+    private void Update()
+    {
+        if (isFalling)
+        {
+            transform.position += (Vector3)(Vector2.down * curentSpeed * Time.deltaTime);
+            curentSpeed += fallAcceleration * Time.deltaTime;
+
+            distancePassed += Mathf.Abs(transform.position.y - previousY);
+            previousY = transform.position.y;
+            if (distancePassed >= fallDistance)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            MoveUpDown();
+        }
+    }
+
+    private void MoveUpDown()
+    {
+        transform.position += (Vector3)(moveDirection * idleSpeed * Time.deltaTime);
+
+        if(transform.position.y >= startY + deltaYIfIdle)
+        {
+            moveDirection = Vector3.down;
+        }
+        else if(transform.position.y <= startY - deltaYIfIdle)
+        {
+            moveDirection = Vector3.up;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Fall();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Fall();
+    }
+
+    private void Fall()
+    {
+        isFalling = true;
+        animator.Play(fallAnimationName);
+        collider.isTrigger = true;
+    }
+}
---
+++ Pl/Assets/Traps/Fan/Fan.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float timeBeetwenSwitch;
+    [SerializeField] private float length;
+    [SerializeField] private string onAnimationName = "On";
+    [SerializeField] private string offAnimationName = "Off";
+
+    private BoxCollider2D collider;
+    private AreaEffector2D effector;
+    private Animator animator;
+    private float timeRemainedToSwitch;
+    private bool isWork;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        effector = GetComponent<AreaEffector2D>();
+        animator = GetComponentInChildren<Animator>();
+
+        length /= transform.localScale.x;
+
+        collider.isTrigger = true;
+        collider.usedByEffector = true;
+        collider.offset = new Vector2(0, length / 2);
+        collider.size = new Vector2(0.25f, length);
+        isWork = true;
+
+        effector.forceAngle = 90;
+        effector.forceMagnitude = isWork ? forceMagnitude : 0;
+
+        timeRemainedToSwitch = timeBeetwenSwitch;
+    }
+
+    private void Update()
+    {
+        if(timeRemainedToSwitch < 0)
+        {
+            Switch();
+        }
+        else
+        {
+            timeRemainedToSwitch -= Time.deltaTime;
+        }
+    }
+
+    private void Switch()
+    {
+        isWork = !isWork;
+
+        animator.Play(isWork ? onAnimationName : offAnimationName );
+        
+        timeRemainedToSwitch = timeBeetwenSwitch;
+        effector.forceMagnitude = isWork ? forceMagnitude : 0;
+    }
+}
